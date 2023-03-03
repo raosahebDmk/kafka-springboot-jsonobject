6a6dda631487b9ca8bf54564945785301c3bbdc7
raosahebDmk <raosaheb.deshmukh88@gmail.com>
2023-03-03 14:52:05 +0530

    added project folder structure

---
+++ src/main/java/com/example/kafkaspringbootjsonobject/KafkaSpringbootJsonobjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringbootjsonobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootJsonobjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringbootJsonobjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaspringbootjsonobject/KafkaSpringbootJsonobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringbootjsonobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootJsonobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
